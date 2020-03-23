45755a5ca92aead7f35d3db974b1079f3fc26f52
syafiqnkholis <syafiqnkh@gmail.com>
2020-03-23 21:23:43 +0700

    tugas card

---
+++ src/Data.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Card from "./Card";
+
+const Data = () =>{
+    return (
+      <div>
+        <Card name="SYAFIQ" job="Programmer"/>
+        <Card name="SYFQ" job="Seniman"/>
+        <Card name="SATIVA" job="Designer"/>
+      </div>
+    )
+  }
+  
+  export default Data;
